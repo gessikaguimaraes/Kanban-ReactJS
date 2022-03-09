63abbfc84d704681e5896c9e41987b0666eac161
Géssika <gessika.156@gmail.com>
2022-03-09 09:49:56 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "./components/Navbar/Navbar";
+import TaskList from "./components/TaskList/TaskList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div>
+        <TaskList title="Pendente" />
+        <TaskList title="Fazendo" />
+        <TaskList title="Completa" />
+      </div>
+    </div>
+  );
+}
